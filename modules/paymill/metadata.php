99130b21cd8ca7a7b91ef47ff403a341be903443
Harenkamp <stefan.harenkamp@payintelligent.de>
2013-06-14 15:26:09 +0200

    - adjust Metadata and pluginconfig

--- modules/paymill/metadata.php
+++ modules/paymill/metadata.php
@@ -4,7 +4,7 @@ $aModule = array(
     'id'           => 'paymill',
     'title'        => 'Paymill',
     'description'  => 'Paymill Payment',
-    'thumbnail'    => 'logo.png',
+    'thumbnail'    => 'image/logo.png',
     'version'      => '2.0',
     'author'       => 'Paymill GmbH',
     'url'          => 'http://www.paymill.de',
@@ -24,8 +24,6 @@ $aModule = array(
     'settings' => array(
         array( 'group' => 'main','name' => 'PAYMILL_PRIVATEKEY', 'type' => 'str', 'value' => ''),
         array( 'group' => 'main','name' => 'PAYMILL_PUBLICKEY', 'type' => 'str', 'value' => ''),
-        array( 'group' => 'main','name' => 'PAYMILL_ACTIVATE_ELV', 'type' => 'bool', 'value' => 'false'),
-        array( 'group' => 'main','name' => 'PAYMILL_ACTIVATE_CC', 'type' => 'bool', 'value' => 'false'),
         array( 'group' => 'main','name' => 'PAYMILL_ACTIVATE_DEBUG', 'type' => 'bool', 'value' => 'false'),
         array( 'group' => 'main','name' => 'PAYMILL_ACTIVATE_LOGGING', 'type' => 'bool', 'value' => 'false'),
         array( 'group' => 'main','name' => 'PAYMILL_ACTIVATE_FASTCHECKOUT', 'type' => 'bool', 'value' => 'false'),
