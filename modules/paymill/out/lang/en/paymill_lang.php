2bb0009621a9a795ee3b1a78d70f176b34cb96b1
Enes Fazli <fazli.enes@googlemail.com>
2013-06-28 16:48:31 +0200

    added structure for english transactions

---
+++ modules/paymill/out/lang/en/paymill_lang.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @category  PayIntelligent
+ * @package   pi_yapital
+ * @copyright (C) 2013 PayIntelligent GmbH  <http://www.payintelligent.de/>
+ * @license http://www.gnu.org/licenses/  GNU General Public License 3
+ */
+$sLangName = "English";
+$aLang = array(
+    'PAGE_CHECKOUT_PAYMENT_NUMBER' => 'Kreditkarte',
+    'PAGE_CHECKOUT_PAYMENT_SECURITYCODE' => 'CVC',
+    'PAGE_CHECKOUT_PAYMENT_HOLDERNAME' => 'Karteninhaber',
+    'PAGE_CHECKOUT_PAYMENT_VALIDUNTIL' => 'Gültig bis',
+    'PAGE_CHECKOUT_PAYMENT_ACCOUNTHOLDER' => 'Kontoinhaber',
+    'PAGE_CHECKOUT_PAYMENT_ACCOUNT' => 'Kontonummer',
+    'PAGE_CHECKOUT_PAYMENT_BANKCODE' => 'Bankleitzahl',
+    'PAYMILL_VALIDATION_CARDNUMBER' => 'Bitte geben Sie eine gültige Kartennummer ein.',
+    'PAYMILL_VALIDATION_ACCOUNTNUMBER' => 'Bitte geben Sie eine gültige Kontonummer ein.',
+    'PAYMILL_VALIDATION_BANKCODE' => 'Bitte geben Sie eine gültige Bankleitzahl ein.',
+    'PAYMILL_VALIDATION_EXP' => 'Bitte geben Sie ein in der Zukunft liegendes Gültigkeitsdatum ein.',
+    'PAYMILL_VALIDATION_CVC' => 'Bitte geben Sie einen gültigen CVC-Code ein.',
+    'PAYMILL_VALIDATION_CARDHOLDER' => 'Bitte geben Sie den Karteninhaber ein',
+    'PAYMILL_VALIDATION_ACCOUNTHOLDER' => 'Bitte geben Sie den Kontoinhaber ein',
+    'charset' => 'UTF-8',
+    'PAYMILL_CC_POWERED_TEXT' => 'Sichere Kreditkartenzahlung powered by',
+    'PAYMILL_ELV_POWERED_TEXT' => 'Sicheres Lastschriftverfahren powered by'
+);
